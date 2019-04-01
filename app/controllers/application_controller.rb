0bc3d9b723ff7ea3a6e38d23817b7378b1743a97
Dmitri Samotessov <dimasamo96@gmail.com>
2019-04-01 04:52:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,11 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
      @user=User.find_by(username: params[:username])
-    # binding.pry
+    if @user
+      session[:user_id]=@user.id
+      redirect '/account'
+    end
+    erb :error
   end
 
   get '/account' do
